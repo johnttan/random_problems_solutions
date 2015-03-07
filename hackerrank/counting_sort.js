5433ee5d19e12c68ce367db3cc2e9024b388b744
John Tan <tantai.john@gmail.com>
2015-03-06 23:58:57 -0800

    Fixed bug with input map

--- hackerrank/counting_sort.js
+++ hackerrank/counting_sort.js
@@ -1,26 +1,28 @@
 function processData(input) {
     var countTable = {};
     var max = 0;
-    input = input.split('\n');
-    input.shift();
+    input = input.split('\n')
     input = input.map(function(el, ind){
-        var elAr = el.split(' ');
-        elAr[0] = parseInt(elAr[0]);
-        countTable[elAr[0]] = 0;
-        if(ind < input.length / 2){
-            elAr[2] = "first";
-        }else{
-            elAr[2] = "second";
+        if(ind > 0) {
+            var elAr = el.split(' ');
+            elAr[0] = parseInt(elAr[0]);
+            if(countTable[elAr[0]] === undefined){
+               countTable[elAr[0]] = 0;
+            }
+            countTable[elAr[0]] ++;
+            if(elAr[0] > max){
+                max = elAr[0];
+            }
+            if(ind < input.length / 2){
+                elAr[2] = "first";
+            }else{
+                elAr[2] = "second";
+            }
+          return elAr
         }
-        return elAr
-    });
-    input.forEach(function(el){
-        countTable[el[0]] ++;
 
-        if(el[0] > max){
-            max = el[0];
-        }
     });
+    console.log(input)
     var subTotal = 0;
     for(var i=0;i<=max;i++){
         var oldCount = countTable[i];
@@ -28,18 +30,22 @@ function processData(input) {
         subTotal += oldCount;
     };
     var results = [];
-    input.forEach(function(el){
-        results[countTable[el[0]]] = el;
-        countTable[el[0]] ++;
+    input.forEach(function(el, ind){
+        if(ind > 0){
+            results[countTable[el[0]]] = el;
+            countTable[el[0]] ++;
+        }
+
     })
 
-    var resultArray = [];
-    results.forEach(function(el){
+    results.forEach(function(el, ind){
         if(el[2] === "first"){
-            resultArray.push('-')
+            process.stdout.write('-');
         }else{
-            resultArray.push(el[1])
+            process.stdout.write(el[1]);
+        }
+        if(ind < results.length-1){
+            process.stdout.write(' ');
         }
     });
-    console.log(resultArray.join(' '))
 }
