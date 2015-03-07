2628d6bfad17d217193aa3ef01bf61e00b25cab5
John Tan <tantai.john@gmail.com>
2015-03-06 18:25:48 -0800

    Implemented mergesort and auxilliary processing

--- hackerrank/counting_sort.js
+++ hackerrank/counting_sort.js
@@ -10,18 +10,35 @@ function processData(input) {
     var numItems = input.shift();
 
     function merge(arr1, arr2){
+        //console.log(arr1, arr2)
+        if(arr1.length === 0){
+            return arr2;
+        }
+        if(arr2.length === 0){
+            return arr1;
+        }
         var resultArr = [];
         var point1 = 0;
         var point2 = 0
-        while(point1 < arr2.length || point2 < arr2.length){
-            if(arr1[point1] >= arr2[point2] || point2 >= arr2.length){
-                resultArr.push(arr[point1]);
+        while(point1 < arr1.length && point2 < arr2.length){
+            if(arr1[point1][0] <= arr2[point2][0]){
+                resultArr.push(arr1[point1]);
                 point1 ++;
-            }else if(arr1[point1] < arr2[point2] || point1 >= arr1.length){
-                resultArr.push(arr[point2]);
+            }else{
+                resultArr.push(arr2[point2]);
                 point2 ++;
             }
         }
+        if(point1 >= arr1.length){
+            for(var i=point2;i<arr2.length;i++){
+                resultArr.push(arr2[i])
+            }
+        } else{
+            for(var i=point1;i<arr1.length;i++){
+                resultArr.push(arr1[i])
+            }
+        }
+        //console.log(resultArr);
         return resultArr;
     };
     function sort(arr){
@@ -29,6 +46,25 @@ function processData(input) {
             return arr;
         }
         var pivot = Math.floor(arr.length / 2);
-        return merge(sort(arr.slice(0, pivot), arr.slice(pivot, arr.length)))
-    }
+        return merge(sort(arr.slice(0, pivot)), sort(arr.slice(pivot, arr.length)))
+    };
+    var results = sort(input);
+    var firstHalf = {};
+    var secondHalf = {};
+    for(var i=0;i<input.length;i++){
+        if(i < input.length / 2){
+            firstHalf[input[i][1]] = true;
+        }else{
+            secondHalf[input[i][1]] = false;
+        }
+    };
+    var resultArray = [];
+    results.forEach(function(el){
+        if(firstHalf[el[1]]){
+            resultArray.push('-')
+        }else{
+            resultArray.push(el[1])
+        }
+    });
+    console.log(resultArray.join(' '))
 }
