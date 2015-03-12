9eb5f67c5a8f7cc3c031b1472237482f8a65894f
John Tan <tantai.john@gmail.com>
2015-03-12 10:53:58 -0700

    Added maximum_sub_array

---
+++ hackerrank/max_sub_array.js
@@ -0,0 +1,27 @@
+function processData(input) {
+  input = input.split('\n');
+  var arrays = [];
+  for(var i=1;i<input.length;i+=2){
+    arrays.push(input[i].split(' ').map(function(el){return parseInt(el)}))
+  };
+
+  arrays.forEach(function(arr){
+    var cSum = 0;
+    var ncSum = 0;
+    var currentSum = 0;
+    arr.forEach(function(el){
+      currentSum += el;
+      if(el > 0){
+        ncSum += el;
+      };
+      if(currentSum < 0){
+        currentSum = 0
+      };
+      if(currentSum > cSum){
+        cSum = currentSum
+      };
+    });
+
+    console.log(cSum, ncSum);
+  })
+}
