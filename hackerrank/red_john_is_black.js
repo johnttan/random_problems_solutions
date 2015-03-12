d69cd1c6ed17f4a7055bdb14e069f4f260c2c0b5
John Tan <tantai.john@gmail.com>
2015-03-12 16:26:31 -0700

    implemented red john is black

--- hackerrank/red_john_is_black.js
+++ hackerrank/red_john_is_black.js
@@ -8,9 +8,43 @@ function generateArranges(N){
   return table[counter-1];
 }
 
+function numPrimes(N){
+  if(N >= 2){
+    var count = 1;
+    var table = [];
+    for(var i=2;i<=N;i++){
+      table.push(i);
+    };
+    var p = 2;
+    while(true){
+      for(var j=0;table[j]<=N;j+=p){
+        table[j] = 0;
+      }
+      var found = false;
+      for(var i=0;i<table.length;i++){
+        if(table[i] > 0){
+          p = table[i];
+          found = true;
+          count ++;
+          break;
+        }
+      }
+      if(!found){
+        return count;
+      }
+    }
+  }else{
+    return 0
+  }
+}
+
 function processData(input) {
   input = input.split('\n').map(function(el){return parseInt(el)});
   for(var i=1;i<input.length;i++){
-    console.log(generateArranges(input[i]))
+    console.log(numPrimes(generateArranges(input[i])))
   }
 }
+
+processData("2\n\
+1\n\
+7")
