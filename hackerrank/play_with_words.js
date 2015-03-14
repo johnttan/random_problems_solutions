3b83b80fc65d11c65f149efbf58c0cc6a6c5bde2
John Tan <tantai.john@gmail.com>
2015-03-14 16:20:46 -0700

    Reduced space complexity

--- hackerrank/play_with_words.js
+++ hackerrank/play_with_words.js
@@ -1,32 +1,44 @@
+
+// The strategy to save space for this problem is to only save the previous row and the current row, as the recurrence relation only depends on i+1 and j-1
+// You also have to save the values in the table associated with the range starting at 0, ending at i and starting at i, ending in 0. This results in 2 arrays of length N each.
+//This only takes O(N) space vs the O(N^2) space of constructing the full table.
 function LPS(someString){
   var table = [];
-  for(var i=0;i<someString.length;i++){
-    var newA = [];
-    table[i] = newA;
-  }
+  var previousRow = [];
 
-  for(var j=0;j<table.length;j++){
-    for(var i=table.length-1;i>=0;i--){
+  var currentRow = 0;
+  var firstSegmentLPS = [];
+  var secondSegmentLPS = [];
+  for(var j=0;j<someString.length;j++){
+    for(var i=someString.length-1;i>=0;i--){
       if(i > j){
-        table[i][j] = 0
+        table[i] = 0
       }
       else if(i === j){
-        table[i][j] = 1
+        table[i] = 1
       }
       else if (someString[i] === someString[j]){
-        table[i][j] = table[i+1][j-1] + 2;
+        table[i] = previousRow[i+1] + 2;
       }else{
         if(i === table.length-1){
-          table[i][j] = table[i, j-1]
+          table[i] = previousRow[i]
         }else if(j === 0){
-          table[i][j] = table[i+1, j]
+          table[i] = table[i+1]
         }else{
-          table[i][j] = Math.max(table[i+1][j], table[i][j-1])
+          table[i] = Math.max(table[i+1], previousRow[i])
         }
       }
+      if(i === 0){
+        firstSegmentLPS[j] = table[i];
+      }
+      if(j === someString.length-1){
+        secondSegmentLPS[i] = table[i];
+      }
     }
+    var previousRow = table;
+    table = [];
   }
-  return table;
+  return [firstSegmentLPS, secondSegmentLPS];
 }
 
 
@@ -38,16 +50,19 @@ function bestFactor(someString){
     if(i === 0 || i === someString.length -1){
       factor = table[0][someString.length-1]
       if(factor > maxFactor){
-        maxFactor = factor
+        maxFactor = maxFactor
       }
     }else{
-      factor = table[0][i] * table[i+1][someString.length-1]
+      factor = table[0][i] * table[1][i+1]
       if(factor > maxFactor){
         maxFactor = factor
       }
     }
   }
   return maxFactor;
+}
+function processData(input){
+  console.log(bestFactor(input))
 }
 // var test = LPS("eeeg");
 // var table = "";
@@ -60,3 +75,12 @@ function bestFactor(someString){
 // console.log(table);
 
 // console.log(bestFactor("eeegeeksforskeeggeeks"))
+// var start = Date.now();
+// processData("lklddhkidgffdkhhkcjkefjlejhkkfihgiiiiddkcihejcdjdkfhjdhkjffjjdcghidgkicffelfekgkljjfclcffjdjgeeeieijhfckcecgfgldghjeicldhfidecglldhjelhfcjeifhjedgflihklfgjjhhcfgglldlhfjlgjicihfigcegifechgkihcchjceifhihjciggihjhgciedgfeffhiegijehjlhkclehiikeegjjfhdfdhkcgglilhhieedgdkfkfhfgekjjfklfgfjhfelekljdkfejgjccggjhcjhcjldcefekgghgkeklkgcfefchegkhgceljkklcgcfllchlddcghhggfhfffhkdefgidfhjlikcidclcedekhghlgejkijkfkfigfccekjcieckkeljhfekdfgidhlchfhlfllldfdfehhdikhlfjjeflkjdhhkkjjhdeecejfchklfhkeieljjhgigdllfidleijghlijkkiijligdfcldfiehhkhglikcggkddhkfgchhjhhkfefejghklfeichcdkfejicfdccjicdkdjigccjlhhkecceghhijgcekefliijilhgijicjhgjkkclcgefhjdjhhjkkkjgkdcfjffleickffdcdhcjcfflgdchfdfkjdecleheiegeehkjgcdflekjfikhhkfikdkejefkelkceklhlcckgecleldjhckiekchkiklecljjgggklighcfjdcgefcgcdlicfhlfgckhfjekejelgckgldfifjifccdckfdiflgkicigfjdihdldcflciicgclkdkgekfigklcjkdjecjecehhkkgifgcgifijljejjckfjeicghfifehjedclcjidicglhjfcdcehhlldjgjjckjlchgdeklkfcfcgggiehelehgjhkhhjigidiicjhcifkekjdkjhddhiicjkkkdjffdglhgckkcgejcihikjcifigdfgfiicecedckglgdfjkdkcelifhjcflhllfgceflilgkcfeldjhljcekljhgfgkgcffgkcddelkgiifhggjkifhceikgcfidfljfkghllgikllkdecikjhgkehkgdlehjkkjgjhjfhhjilhkhlkiedjlkkfkhhjlhdjdccdkcfihcgghigfgkcdkcjjjghfelcjgigfccikigijcdkgicgeedhffhfkgkcjdcjkgfjehfgjiefickchgcfchkcldjfhlfilfkfldhekhjdijkhglhgkiilihkhgdehjkigjfdfjgghckkddjcffcjiciidhggilichkjldljegjkfdjfcejldegjeecfljfldldkiidkffclkedljheejhgjceeglhlkflefhdgjfdcileecigeccgdifcicfkhliflfhhgjecdfccgdigffkgdkilfijceelhfljfijiccdklkclccgilgkhekggdcgjhjccdkldccicckkdcchgghgjhdfghfclclikkdfdffieflkcchdjdgcdjilgdlkegikceghikfdffeglkhkfilchhekhdicchkclciceddccekihcfhfgjcfkghceigfjglfdheciehcdfhcgfkdiiclhiceifcgidejcklidiieckkfdkkkejigljhgffgjjldffddfjdgcghkeicidgdhcjkjehgdhehlhghljfddgiiclgeijcikeckedffgihfledkfcfefffldkfkhdlkfcdhlikefhkdlflgcgkickfffhigfggkfccjdjedkedigjdhjcgcggfijgjgdkejhijigfgkfljldhdffkkkliiedhiegkfjkgigfdcdegeilfkcegekglcjkidjffceiekdkhedkkfefjidhfhgkiiillielhdfgclflcjhkjhcldfhcejcdkdlkckhdfdlchilcgkheliflkkdkcggilejfdciekhgldggffgkfjejeljhfkljkgjfchcfdlihjjihgigilfjcjgcjjcflhjdleijeghgielckifikejglkekcfjcljdeeckddkjgkfidlcfkgdjjclfjlcfjjekffhickllfigddjfdcgdllgekjkjhldikighjdecfhhigchleechihjkhlkiihdfcjcjhjliedjhligediehihecicckccgghdilffifkgflljlckdkjcikggchgggfidchjijhjhcfefdglkidjgefcegkdcgfjckejgjdgihhdhflceciilfkkckdfgihcefllgceihgjecikceldgcfjcjgjhllkelgfelhhefhkdccdgigdiddccehjjecjdlkjcllhegfgllflcgcccgcfdkhdjejikcfgejhkdeldkhcflfcgdhhfdkikkkhkhgegkcjggcjlifkedcfciidkghhlhjcjhjcdddilkeficdfcekidfecldddjf");
+// var stop = Date.now();
+// console.log(stop - start);
+
+// var start = Date.now();
+// processData("zyyxxyzxzxzyzyxzzyxxzxyzyyzxzzzyyxyyzzzxzzxxzzxzxyzzzxzyxxxzxzxxxyyxyxxxzxzyxyyxxxzxxzyxxxxyxyyzzxxxxyxyzxxyyzxxzyzxxzyzyxyxyyxyxyzxzzzzyxxzxxzxzxyyxyyzxyyyxyyxzxxyzzyzyyyyzyzxzyxyxzxxyyzzxzzzyyzyyzzzyyxxxzxxyyzyyxxyzzxxzzxzyyzyyzzyzzxyxyxxzyxxyxxxxyyyyzzyzxyzzzyxyyxzzxyyzyzxxzzyzyzzzzxyzzyyxxzyzxzyxxzyyzzxxzxyzxyyzzyxyzyzzxxzzyyzzxxxxyzzxxyzzyyyzxzzyzxyyxxzzxyxxzzzxzxyyzyxzzxzyzyzyzxzyxzyzzyyyzyyxzzyzyyzyyxyxyzzzzyyzzyyyxzxyxyyyzzzzyzzzxxzyzxzzxyzxzzzzzzyyzzyyzyxxzzyyyxzyyyyxxxxzxzzxzzzyxxxzxzxyzzxxyzzxzzxzxyyzyyyxyxyxxyxxzxxzzyzxyzzzyzzxyxzxzxzzzyyzyyzyxzyyzyzyxyyyyxzxyxxyyzxzyyzxzzyxyyxyyzxzzzyyzyxyyxzyxzxyxyyxyyxyyzzxzyxzzyxxzxxzxyyyyxxyxxzzxxyyyyzyyxzzzxzyzyxzzyyzxxyzyyyyxzzyxxzyyyxxzzyyxzyzzyxxxxzxyzxzzxyyxzxxxxxxxzyxzxzzxzyyyzxxyyzyzzyzzyyzxyxzzzxyxxzxzyyyxxxxyzxzxzyxxzxzyzzxzxzyyzyzyyyzxyxyzyyxyxxxyyzyzyyxxyyxzyxxyzzyxxyzyxxzxzyyyxxyxxzxzzxxyxyyzyyxxxxyxzzxxyxzzyyyyxxzyyxxzyzyyzzyxxxyzzxyxyzxyyyzzxzyyzxxzxxyxxxzyzxxyyxzyyyyxxyzyxxyzxyxxyxzxzyyyyyzzxxyxyyxyzzzzxyyyxxxyxzxyyyzzzxzzyzyxyyyxzxzzzyyyyyzyzzyzxyzxxzyyzzzzyzzxxxzxyyzxxzxxzzyxzyxyzyzzxzyzxzxzzxxyzzyzzxyzyxyxxyxzzzyxxxzyzzyxxzzxyyxyxzzzzzzxzxyxzzxyyxzyyxyzyxzxyzxzyxyzyxxxzyyzzzzyzyxzxzxyyzzyzyzxyyzzzyyxyzzyzxyzyyzzxxxzyzyxzxzxyzyzyzxzxzyxxxyyyyzyzyyxzxxzzxzzzzzyxzzyyyyzyxxzzxzzyzzyxyzxzyxyyzzxxzzzyxyyxxyzzyzxyzzzxyzxyxzzxzyzyyxzxzxzzzzxxxxyyxyyxyyyxzzzxyzyzzyxxzzxzzyxxyyyyzzxyyyzzyzyzzzzzxzzxzzzyzyzzxyzzyxyxyzxxxyzyzyzyzxzyxyyzzyzzzxyzxxzzyyxzxxyxxyzyxzxzxyyzxyxxyzyxxyyyzyyzxyxzzxxxzxzyyyxxzxyzyzxyyzzzxzyxzzxxzyxzxzyxzxyxzxzzxxyzxxyyzxyzxxyyzzyzxzxyxzxxyyyzzzzzzyxxxxzzxzzxzyxzxzxyxyxzyyzyxxxzzzzzyxxyzzxyyzyzzyyzzyzxzyyxxzyyzyyzzyyyyzyxxxzxxxyyzxyyyzxzxxzyzzzxzxzyyyyyyxyxyxzxzxxxxxyyzzzyyxzzzyyxxyzxxxxzzzyyyzxzzzyxyzyzyzyxxxzxzyzzyxyzxyyyxzxxyzyxxyxzzxzyyyzxxyxyxyyzxzxyyyzyxyxxxxzzxxxyzxyzxzxxzxyzxxyyxxyyzzyzyzxxzzyzzzxzyzzyzyyxyxzxxxyyxyxzzxxxzzyxyyyyyzxzxxyxzxyyyxzzyyzzyyxzxyxyxzxxxyxxxzxzzzzxyxyyxxyxyxyxxzxyyyyxxxyzyxzyxxyzxxxxyzxyzyzxyyyxzyzzzxxxyyxxzyyzxxxxyzyxzyxzxxyzxyyxzyzxzyzxzzzxyyxzxyyxxzyxyxyyzzyxyzzzyxxzzzzzyxzzzzxxyxxyyyxyyzzxzxxzxzzzxxzxyyyxzyxyzyyzyzzxyxyxyyxxxyyxyxyxzzzyxzxzzxzyyxxzxyzyxzyzzzzyxxzzzxxyyyxyzzzxyzzxxyyxxzxxxxyyyyxzzzxxzyxzyxxxxyxyyyxzxxyzyxzxxzxxyyyzyzxyzzzxzxyxzyxxzxyxxxxzyyyzyzxxzxyzzyzxyzyzzyzxxxyyxyxzxyyxzyzxyxyxzxxyxzyzzzzzxxyzyzxzyzyxzxzxyxxyzxyzyzyxxxzzxzxxzxzzxzyxyyyxxxzyzyzzyyzzzyxzyyxyxyzzyxyzzyyxxxzyzyxyzxzzyyyyzzzzxyxyxxyyyzyyyxyxyyxyxxzyzzzyyzxxzxyzxzzzyyzxyyxzzyzyyyyyyxxyzyxyxyxxyxyxyxxxxzzzzzzyxzyyzxyzyyyyyyxyxxxyxzyyzxzyyxyyyyzyzxyzzzzzyxzxzzyxyyzzyyyzyxyyxzxyxzxyyyxyyxzxyxzzzxyyxyyzxyxyzxyxxxzxyzzxyyzzzyyzxxyyxyyzyyxxxyzzxyxxzxxyzzxzzyxxzyzxxxyzxyyzzxyxxyxzxxzxzzxxxxyxxzyyxyyyzxzyyyyyxyxzzyxzzzyyzxyyxyxxzzxyyzyyyzxzxxzxyxxxyyyyyxzyzxxyxzzxzyzxzxzyzxyyxxxyxyxyzzzyxzxxzxzzxyxyyzzzyzxzyyxxxyzzxxzxzzxxzyyyzyxxyyxyyxzzxzzzxxyxxyzzxyzxxzzyyzyyyyzzxzzzyzzzyxxyxyxyyzxzxyzzxxyxzyyyxyxxzzyyzzzxyzxyyxxxxxxzyzzyzyyzxzxyyzxyyxzyxzxxyzzxyyxyxzzxxyyyzyxzzzxyyxzxxyzxxyyyxzyyxzzxxxxzyzzyxyxyyxxyxxyzzxyzyyzxyzyzxyxyyzzxyzzyxxxxyzxxzzzxzyxyxxzxxxxzxxyzz");
+// var stop = Date.now();
+// console.log(stop - start);
