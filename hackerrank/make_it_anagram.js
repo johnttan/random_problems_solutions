20335bb866f0620d0fc0fd16d6a3d3163a9abeab
John Tan <tantai.john@gmail.com>
2015-03-15 23:37:32 -0700

    fixed bug

--- hackerrank/make_it_anagram.js
+++ hackerrank/make_it_anagram.js
@@ -3,11 +3,13 @@ function processData(input) {
   var set = {};
   var common = 0;
   for(var i=0;i<input[0].length;i++){
-    set[input[0][i]] = true;
+    set[input[0][i]] = set[input[0][i]] || 0;
+    set[input[0][i]] ++;
   };
   for(var j=0;j<input[1].length;j++){
-    if(set[input[1][j]]){
+    if(set[input[1][j]] > 0){
       common ++;
+      set[input[1][j]] --;
     }
   }
   console.log(input[0].length - common + input[1].length - common);
