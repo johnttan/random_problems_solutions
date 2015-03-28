de3a28a9f9c27128bd55ea3615abbdf3a70cc497
John Tan <tantai.john@gmail.com>
2015-03-27 19:58:49 -0700

    added counting

--- hackerrank/even_tree.js
+++ hackerrank/even_tree.js
@@ -3,14 +3,17 @@ var graph = {};
 function processData(input) {
   input = input.split('\n');
 
-  input.forEach(function(pair){
-    pair = pair.split(' ');
-    graph[pair[0]] = graph[pair[0]] || [];
-    graph[pair[1]] = graph[pair[1]] || [];
-    graph[pair[0]].push(pair[1]);
-    graph[pair[1]].push(pair[0]);
+  input.forEach(function(pair, ind){
+    if(ind > 0){
+      pair = pair.split(' ');
+      graph[pair[0]] = graph[pair[0]] || [];
+      graph[pair[1]] = graph[pair[1]] || [];
+      graph[pair[0]].push(pair[1]);
+      graph[pair[1]].push(pair[0]);
+    }
   });
   var visited = {};
+  var done = {};
   var size = {};
   var dfsStack = [];
 
@@ -18,20 +21,45 @@ function processData(input) {
 
   while(dfsStack.length > 0){
     var current = dfsStack.pop();
+    console.log(current)
     if(!visited[current]){
       dfsStack.push(current);
+      visited[current] = true;
+      graph[current].forEach(function(v){
+        if(!visited[v]){
+          dfsStack.push(v);
+        }
+      });
     }else{
       var sumOfChildren = 0;
       graph[current].forEach(function(child){
-        sumOfChildren += size[child];
+        if(done[child]){
+          sumOfChildren += size[child];
+        }
       })
       size[current] = sumOfChildren + 1;
+      done[current] = true;
     };
-    visited[current] = true;
-    graph[current].forEach(function(v){
-      if(!visited[v]){
-        dfsStack.push(v);
-      }
-    });
-  }
+  };
+
+  var edgeCount = 0;
+
+  for(var v in size){
+    if(size[v] % 2 === 0 && v !== "1"){
+      edgeCount ++;
+    }
+  };
+
+  console.log(edgeCount);
 }
+
+processData('10 9\n\
+2 1\n\
+3 1\n\
+4 3\n\
+5 2\n\
+6 1\n\
+7 2\n\
+8 6\n\
+9 8\n\
+10 8')
