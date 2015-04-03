5e8dab876594c46c1226cbdd83c0f7f12253dd75
John Tan <tantai.john@gmail.com>
2015-04-03 14:42:02 -0700

    added comments

--- hackerrank/stock_maximize.js
+++ hackerrank/stock_maximize.js
@@ -1,3 +1,10 @@
+/*
+Stock orders as set of pairs (i, j) where j > i; i, j in range N days; i = buy day, j = sell day
+Best strategy is to always buy at i and sell at j where j is max price in range i, N-1;
+You will never choose to sell at j where it may have been better to buy at j because for k in range j, N-1, if there exists k where k > j, then for all p in i, j, p would have sold at k instead of j
+In addition, if you were to buy at j following our strategy, there must be a k in j, N-1 where k is max in that range, and no buy order in 0, j sold at j, which means that j is not the max in j, N-1, and thus there was a better choice than j to sell at.
+*/
+
 function solve(prices){
   var profits = 0;
   var maxPrice = [];
